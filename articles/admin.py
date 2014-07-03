99bcf1b0d5e65c6ea9d73e4e6ade340ef1cc6a94
Andy Bulka <abulka@gmail.com>
2014-07-03 23:46:08 +1000

    enabled admin interface

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from articles.models import Article
+from articles.models import Fred
+
+admin.site.register(Article)
+admin.site.register(Fred)
+
+"""
+My db password for the admin interface is
+andy / andy
+
+"""
